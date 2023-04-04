2c6bd532de46347f6a3d96c1a52d9fb5de8fee59
Fahla <nunofahla@ua.pt>
2023-04-04 23:13:59 +0100

    improved entities pagination

--- entities/app.py
+++ entities/app.py
@@ -2,14 +2,10 @@ import uuid
 from flask import *
 import json
 import sqlite3
-from pymongo import MongoClient
-# ver sql alchemy
+import os
+
+
 app = Flask(__name__)
-
-client = MongoClient('localhost', 27017)
-
-mdb = client.m_entities
-todos = mdb.todos
 
 class Entity:
 
@@ -26,13 +22,13 @@ class Entity:
         self.sku_list = sku_list
 
     def __str__(self):
-        return  f'id:{self.id} \n' \
-                f'name:{self.name} '
+        return  f'{{id:{self.id} \n' \
+                f'name:{self.name}}}'
 
 def db_init():
     c = sqlite3.connect("entities.db").cursor()
-    c.execute("CREATE TABLE IF NOT EXISTS ENTITIES("
-              "id TEXT, approved TEXT, isPartner TEXT, name TEXT, address TEXT, description TEXT, homePage TEXT, phoneNo TEXT, nif TEXT, sku_list TEXT)"
+    c.execute("CREATE TABLE IF NOT EXISTS entities("
+              "id TEXT PRIMARY KEY, approved TEXT, isPartner TEXT, name TEXT, address TEXT, description TEXT, homePage TEXT, phoneNo TEXT, nif TEXT, sku_list TEXT)"
               )
     c.connection.close()
 
@@ -47,12 +43,28 @@ def get_entities():
     page_size = request.args.get('page_size', default=10, type=int)
     offset = (page - 1) * page_size
     in_offset = request.args.get('offset', default=offset, type=int)
+    srch = request.args.get('searchName', default="", type=str)
 
 
     c = sqlite3.connect("entities.db").cursor()
-    c.execute(f'SELECT * FROM ENTITIES LIMIT {page_size} OFFSET {in_offset}')
+    query = f"SELECT * FROM entities WHERE name LIKE ? LIMIT ? OFFSET ?"
+    c.execute(query, ('%' + srch + '%', page_size, in_offset))
     data = c.fetchall()
-    return jsonify(data)
+
+    entities = entitiesToJson(data)
+    metadata = {
+        'searchName': srch,
+        'page': page,
+        'offset': in_offset,
+        'count': len(entities)
+    }
+
+    response = {
+        'pagination': metadata,
+        'entities': entities
+    }
+
+    return jsonify(response)
 
 @app.route('/v1/entity', methods=['POST'])
 def add_entity():
@@ -68,7 +80,7 @@ def add_entity():
                       request.json["sku_list"])
     
 
-    c.execute("INSERT INTO ENTITIES VALUES(?,?,?,?,?,?,?,?,?,?)",
+    c.execute("INSERT INTO entities VALUES(?,?,?,?,?,?,?,?,?,?)",
               (entity.id, 0, entity.isPartner, entity.name, entity.address, entity.description, entity.homePage, entity.phoneNo, entity.nif, entity.sku_list))
     db.commit()
     data = c.lastrowid
@@ -77,16 +89,18 @@ def add_entity():
 @app.route('/v1/entity/<id>', methods=['GET'])
 def getEntitiesById(id):    
     c = sqlite3.connect("entities.db").cursor()
-    c.execute("SELECT * FROM ENTITIES WHERE id=?", (id,))
+    c.execute("SELECT * FROM entities WHERE id=?", (id,))
     data = c.fetchone()
-    return json.dumps(data)
+
+    entt = entityToJson(data)
+    return entt
 
 @app.route('/v1/entity/<id>', methods=['PUT'])
 def approveEntity(id):
     db = sqlite3.connect("entities.db")
     c = db.cursor()
-    if request.form["approved"] == 1 :
-        c.execute("UPDATE ENTITIES SET approved = ? WHERE id=?", (1, id))
+    if request.form["approved"] == "1" :
+        c.execute("UPDATE entities SET approved = ? WHERE id=?", (1, id))
         db.commit()
         return json.dumps("Entity was successfully approved")
     return json.dumps("Couldn't approve entity")
@@ -95,7 +109,7 @@ def approveEntity(id):
 def removeEntity(id):
     db = sqlite3.connect("entities.db")
     c = db.cursor()
-    c.execute("DELETE FROM ENTITIES WHERE id=?", (id))
+    c.execute("DELETE FROM entities WHERE id=?", (id))
     db.commit()
     return json.dumps("Entity was successfully removed")
 
@@ -103,11 +117,11 @@ def removeEntity(id):
 def addSKU(id, skuid):
     db = sqlite3.connect("entities.db")
     c = db.cursor()
-    c.execute("SELECT * FROM ENTITIES WHERE id=?", (id))
+    c.execute("SELECT * FROM entities WHERE id=?", (id))
     skus = c.fetchone().sku_list
     if skuid not in skus:
         skus.append(skuid)
-        c.execute("UPDATE ENTITIES SET sku_list = ? WHERE id=?", (skus, id))
+        c.execute("UPDATE entities SET sku_list = ? WHERE id=?", (skus, id))
         db.commit()
         return json.dumps("SKU successfully added   ")
     return json.dumps("SKU already associated to partner")
@@ -116,14 +130,23 @@ def addSKU(id, skuid):
 def removeSKU(id, skuid):
     db = sqlite3.connect("entities.db")
     c = db.cursor()
-    c.execute("SELECT * FROM ENTITIES WHERE id=?", (id))
+    c.execute("SELECT * FROM entities WHERE id=?", (id))
     skus = c.fetchone().sku_list
     if skuid in skus:
         skus.remove(skuid)
-        c.execute("UPDATE ENTITIES SET sku_list = ? WHERE id=?", (skus, id))
+        c.execute("UPDATE entities SET sku_list = ? WHERE id=?", (skus, id))
         db.commit()
         return json.dumps("SKU successfully removed")
     return json.dumps("SKU not associated to partner")
 
+def entitiesToJson(data):
+    prdtcs = [{'id': row[0], 'approved': row[1], 'isPartner': row[2], 'name': row[3], 'address': row[4], 'description': row[5], 'homePage': row[6], 'phoneNo': row[7], 'nif': row[8], 'sku_list': row[9] } for row in data]
+    return prdtcs
+
+def entityToJson(row):
+    entty = {'id': row[0], 'approved': row[1], 'isPartner': row[2], 'name': row[3], 'address': row[4], 'description': row[5], 'homePage': row[6], 'phoneNo': row[7], 'nif': row[8], 'sku_list': row[9] }
+    return jsonify(entty)
+
 if __name__ == '__main__':
     app.run(port=8888)
+
